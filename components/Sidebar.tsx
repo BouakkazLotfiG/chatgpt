0cf8e8b8c5be89d960800e8d8a4b96a22d0dfc4b
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-06-29 21:17:59 +0100

    fixed oerder

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,15 +6,14 @@ import NewChat from './NewChat';
 import { signOut, useSession } from 'next-auth/react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '@/firebase';
-import { collection, orderBy, query } from 'firebase/firestore';
+import { collection, query } from 'firebase/firestore';
 import ChatRow from './ChatRow';
 
 const Sidebar = () => {
   const { data: session } = useSession();
 
   const [chats, loading, error] = useCollection(
-    session && query(collection(db, 'users', session.user?.email!, 'chats')),
-    orderBy('createdAt', 'asc')
+    session && query(collection(db, 'users', session.user?.email!, 'chats'))
   );
 
   return (
