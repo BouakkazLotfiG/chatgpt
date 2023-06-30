25c3c0b65b025b484fd81267c619dcccfa66578e
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-06-30 16:32:42 +0100

    displaying response correctly

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,9 +1,46 @@
+'use client';
+
+import { db } from '@/firebase';
+import { collection, orderBy, query } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import Message from './Message';
+
 type Props = {
   chatId: string;
 };
 
-const Chat = ({ chatId }: Props) => {
-  return <div className='flex-1'>Chat</div>;
-};
+function Chat({ chatId }: Props) {
+  const { data: session } = useSession();
+
+  console.log('Session:', session);
+  console.log('Email:', session?.user?.email);
+  console.log('Chat ID:', chatId);
+
+  const [messages] = useCollection(
+    session &&
+      query(
+        collection(
+          db,
+          'users',
+          session?.user?.email!,
+          'chats',
+          chatId,
+          'messages'
+        ),
+        orderBy('createdAt', 'asc')
+      )
+  );
+
+  console.log(messages?.docs);
+
+  return (
+    <div className='flex-1'>
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
+    </div>
+  );
+}
 
 export default Chat;
--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -25,7 +25,7 @@ const ChatRow = ({ id }: props) => {
     if (!pathname) return;
 
     setActive(pathname.includes(id));
-  }, [pathname]);
+  }, []);
 
   const removeChat = async () => {
     await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id));
